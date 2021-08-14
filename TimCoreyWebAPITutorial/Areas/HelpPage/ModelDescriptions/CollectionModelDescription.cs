dfd79153d746d13f7981c81c06894202002bd091
edward.dupre <edward.dupre@redcross.org>
2021-08-13 20:19:34 -0400

    add PeopleController.cs

---
+++ TimCoreyWebAPITutorial/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TimCoreyWebAPITutorial.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
